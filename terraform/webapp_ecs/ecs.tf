595c615ea42f015bd74dee04aae984088f4d7b93
Mavin <vincenzo.marragony@gmail.com>
2021-02-18 14:43:52 +0100

    feat: ecs and iam completition

--- terraform/webapp_ecs/ecs.tf
+++ terraform/webapp_ecs/ecs.tf
@@ -1,4 +1,21 @@
+module "ecs-container-definition" {
+  source  = "cloudposse/ecs-container-definition/aws"
+  version = "0.49.2"
+
+  container_image = "arn:aws:ecr:${var.region}:${var.user_id}:repository/${var.ecr_registry_name}"
+  container_name = "backend"
+}
+
 resource "aws_ecs_cluster" "webapp_ecs_cluster" {
   name = "webapp_ecs_cluster"
   capacity_providers = ["FARGATE"]
+}
+
+resource "aws_ecs_task_definition" "webapp_ecs_backend" {
+  family = "webapp_ecs_backend"
+  container_definitions = module.ecs-container-definition.json_map_encoded_list
+  requires_compatibilities = ["FARGATE"]
+  network_mode = "awsvpc"
+  cpu = "512"
+  memory = "1024"
 }
--- terraform/webapp_ecs/iam.tf
+++ terraform/webapp_ecs/iam.tf
@@ -2,6 +2,10 @@ resource "aws_iam_user" "webapp_ecs_user" {
   name = "webapp_ecs_user"
 }
 
+resource "aws_iam_access_key" "webapp_ecs_user_access_key" {
+  user = aws_iam_user.webapp_ecs_user.name
+}
+
 resource "aws_iam_user_policy" "webapp_ecs_user_policy_s3" {
   user = aws_iam_user.webapp_ecs_user.id
   policy = data.aws_iam_policy_document.s3_access.json
@@ -10,4 +14,12 @@ resource "aws_iam_user_policy" "webapp_ecs_user_policy_s3" {
 resource "aws_iam_user_policy" "webapp_ecs_user_policy_ecr" {
   user = aws_iam_user.webapp_ecs_user.id
   policy = data.aws_iam_policy_document.ecr_power_user.json
+}
+
+output "access_key_id" {
+  value = aws_iam_access_key.webapp_ecs_user_access_key.id
+}
+
+output "access_key_secret" {
+  value = aws_iam_access_key.webapp_ecs_user_access_key.secret
 }
